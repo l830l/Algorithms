ca7614c7659a4b0b64687f5167a434991ea57a66
l830l <principle950@daum.net>
2026-05-18 17:11:35 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 8.13 ms, Memory: 60.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        Set<Integer> answer = new TreeSet<>();
+
+        for(int i = 0; i < numbers.length - 1; i++){
+            for(int j = i + 1; j < numbers.length; j++){
+                answer.add(numbers[i] + numbers[j]);
+            }
+        }
+
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
+}
